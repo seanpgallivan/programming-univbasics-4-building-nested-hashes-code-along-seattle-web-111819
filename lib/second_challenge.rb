dc1e65c2c7cbd90c9b0008b9c3ab5fe2be795b9e
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-13 23:33:09 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,10 +9,7 @@ def second_challenge
     :capulet => {
       :patriarch => 
       :matriarch =>
-      :hero =>
-      :hero_friends =>
-
+      :heroine =>
+      :heroine_friends =>
       }, 
-
-  
 end
