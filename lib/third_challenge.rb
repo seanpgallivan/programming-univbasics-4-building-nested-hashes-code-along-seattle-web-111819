5398ddab781ae0c367a2823695c3098d027c8093
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-13 23:37:56 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,19 +8,30 @@ def third_challenge
         :matriarch => {
           name: "Lady Montague",
           age: "54"
-          
         },
-        :hero => {name: "Romeo", age: "15", status: "alive"},
+        :hero => {
+          name: "Romeo",
+          age: "15",
+          status: "alive"
+        },
         :hero_friends => []
      },
      :capulet => {
-        :patriarch => {name: "Lord Capulet", age: "50"},
-        :matriarch => {name: "Lady Capulet", age: "51"},
-        :heroine => {name: "Juliet", age: "15", status: "alive"},
+        :patriarch => {
+          name: "Lord Capulet",
+          age: "50"
+        },
+        :matriarch => {
+          name: "Lady Capulet",
+          age: "51"
+        },
+        :heroine => {
+          name: "Juliet",
+          age: "15",
+          status: "alive"
+        },
         :heroine_friends => []
      }
   }
 }
-
-  
 end
